9449a94d44100672474eb202175c3c2603fac9e9
Emna Guellaty <eguellaty@gmail.com>
2021-05-17 16:12:01 +0200

    setup end

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-require("dotenv").configs()
+require("dotenv").config()
+require("./configs/mongo");
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,7 +7,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/user');
 
 var app = express();
 
