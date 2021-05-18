0ce8969759f86e71f1dd7aa97d01893e3e1f3119
Emna Guellaty <eguellaty@gmail.com>
2021-05-18 15:33:35 +0200

    auth fixed

---
+++ middlewares/exposeFlashMessage.js
@@ -0,0 +1,1 @@
+module.exports = function exposeFlashMessage(req, res, next) {  res.locals.success_msg = req.flash("success");  res.locals.error_msg = req.flash("error");  res.locals.warning_msg = req.flash("warning");  next();};
