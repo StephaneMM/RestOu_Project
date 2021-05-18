60eeb256bb0a35edb7ea7324add6a7dcce3019a2
Quentin Chauvat <quentin.chauvat@gmail.com>
2021-05-18 14:34:13 +0200

    routes auth

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const session = require("express-session");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/user');
 var restaurantRouter = require('./routes/restaurant');
+var authRouter = require('./routes/auth');
 var app = express();
 
 // INITIALIZE SESSION
@@ -33,6 +34,9 @@ app.use(require("./middlewares/exposeLoginStatus"));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+hbs.registerPartials(__dirname + "/views/partials");
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -42,6 +46,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/restaurants', restaurantRouter);
+app.use(authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = new express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const RestaurantModel = require("./../models/Restaurant");
 const ReviewModel = require("./../models/Review");
 const UserModel = require("./../models/User");
+const protectRoute = require("./../middlewares/protectRoute");
+const protectAdminRoute = require("./../middlewares/protectAdminRoute");
 
 router.get("/signin", (req, res, next) => {
-    res.render("auth/signin.hbs");
+    res.render("./../views/auth/signin.hbs");
 });
 
-router.get("signup", (req, res, next) => {
-    res.render("auth/signup.hbs");
+router.get("/signup", (req, res, next) => {
+    res.render("./../views/auth/signup.hbs");
 });
 
 router.post("/signin", async (req, res, next) => {
@@ -20,19 +22,19 @@ router.post("/signin", async (req, res, next) => {
     console.log(foundUser);
     if(!foundUser) {
         req.flash("error", "Invalid credentials");
-        res.redirect("/auth/sigin");
+        res.redirect("/signin");
     } else {
         const isSamePassword = bcrypt.compareSync(password, foundUser.password);
         console.log(isSamePassword);
         if(!isSamePassword) {
             req.flash("error", "Invalid credentials");
-            res.redirect("/auth/signin");
+            res.redirect("/signin");
         } else {
             const userObject = foundUser.toObject();
             delete userObject.password;
             req.session.currentUser = userObject;
             req.flash("success", "Successfully logged in ....");
-            res.redirect("/");
+            res.redirect("/index");
         }
     }
 
@@ -45,14 +47,14 @@ router.post("/signup", async (req, res, next) => {
       const foundUser = await UsersModel.findOne({ email: newUser.email });
       if (foundUser) {
         req.flash("warning", "Email already registered");
-        res.redirect("/auth/signup");
+        res.redirect("/signup");
       } else {
         const hashedPassword = bcrypt.hashSync(newUser.password, 10); //
         // console.log(newUser.password, hashedPassword);
         newUser.password = hashedPassword; //setting hashedpassword as the new password
         await UsersModel.create(newUser); // creating a user inside de user database with encrypted password instead of the regular one
         req.flash("success", "Congrats ! You are now registered !");
-        res.redirect("/");
+        res.redirect("/index");
       }
     } catch (err) {
       let errorMessage = "";
@@ -61,14 +63,14 @@ router.post("/signup", async (req, res, next) => {
         errorMessage += err.errors[field].message + "\n";
       }
       req.flash("error", errorMessage);
-      res.redirect("/auth/signup");
+      res.redirect("/signup");
     }
   });
 
 
 router.get("/signout", (req, res, next) => {
-    req.session.destroy(funtion (err) {
-        res.redirect("/auth/signin");
+    req.session.destroy(function (err) {
+        res.redirect("/signin");
     });
 });
 
