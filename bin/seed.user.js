693d2abc54e638a9b7150102e8e87d3666b4816e
Quentin Chauvat <quentin.chauvat@gmail.com>
2021-05-19 15:54:19 +0200

    seed for users and reviews

--- bin/seed.user.js
+++ bin/seed.user.js
@@ -3,5 +3,50 @@ require("./../../configs/mongo");
 
 const UserModel = require("./../models/User");
 const RestaurantModel = require("./../models/Restaurant");
+const { IncomingMessage } = require("http");
 
 // const xxxxx = []
+/*const userSchema = new Schema(
+  {
+    isAdmin: Boolean,
+    firstName: String,
+    lastName: String,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if(!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
+        }
+      },
+    },
+    password: String,
+    address: String,
+    favoris: [{ type: Schema.Types.ObjectId, ref: "restaurant" }],
+  },
+  { timestamps: true }
+);*/
+
+let users = [
+    {
+    isAdmin: true,
+    firstName: 'Quentin',
+    lastName: 'Chauvat',
+    email: 'quentin.chauvat@ieseg.fr',
+    password: '$2a$10$sbs8aj1Q1PrGKhMkkyGvSOeRCubDzCr1JlVkpgsGt3ys3nGn2fngK',
+    address: '3 rue camille tahan Paris 75018',
+    favoris: [],
+    },
+    {
+        isAdmin: true,
+        firstName: 'Stephane',
+        lastName: 'Mbesse',
+        email: 'stephane.mbesse@gmail.com',
+        password: '$2a$10$sbs8aj1Q1PrGKhMkkyGvSOeRCubDzCr1JlVkpgsGt3ys3nGn2fngK',
+        address: '3 rue camille tahan Paris 75018',
+        favoris: [],
+        }
+]
